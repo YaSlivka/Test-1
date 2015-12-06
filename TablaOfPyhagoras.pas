8165e97e953fed2bf86518267815e368bccf72fc
YaSlivka <Dmitriy.Brunetkin@yandex.ru>
2015-12-07 00:29:54 +0500

    Все нормально
    Наконец-то

--- TablaOfPyhagoras.pas
+++ TablaOfPyhagoras.pas
@@ -1,11 +1,13 @@
 Program Tabl_MediumMod_harder;           
 var x,z,y,f,d,b,p,k:integer;
 var q:string;
-procedure calc(var z,x,y:integer;var q:string);
+procedure calc(var x,y:integer);
+var z:integer;
+var q:string;
   begin
     z:=x*y;
     q:=IntToStr(z);
-    write('',q,' ');
+    write('',q,'.');
      if y=10 then
      writeln
   end;
@@ -15,7 +17,7 @@ procedure first(var x,y:integer);
            begin
              for y:=1 to 10 do
                begin
-                 calc(z,x,y,q);           
+                 calc(x,y);           
                end;
            end;
        end;
@@ -25,23 +27,30 @@ procedure second(var d,b:integer);
            begin
              while (b>=1) and (b<=10) do
               begin
-                calc(z,d,b,q); 
+                calc(d,b); 
+                b:=b+1; //inc(b);
               end;
+              d:=d+1;
+              b:=1;
            end;
       end;
 procedure third(var p,k:integer);
       Begin 
         repeat  
           Begin 
-            repeat 
+            p:=p+1;k:=1;
+            repeat
               Begin
-                calc(z,p,k,q);
+                calc(p,k);
+                k:=k+1;
               end;
-            until (k<1) and (k>10)
+            until (k<1) or (k>10);
           end;
-        until (p<1) and (p>10)
+        until (p<1) or (p>=10);
+        
       end;
 Begin
+d:=1;b:=1;p:=0;k:=1;
 Writeln('Введите номер способа, 1-For, 2- While,3-until');
 readln(f);
 If f=1
